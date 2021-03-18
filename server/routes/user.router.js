2eafce4c2393cd6035807982d75fdc5a35ecf156
Zach Mead <zachary.n.mead@gmail.com>
2021-03-18 11:47:23 -0500

    Halfway through router.put

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -20,22 +20,42 @@ router.get('/', rejectUnauthenticated, (req, res) => {
   // Send back user object from the session (previously queried from the database)
   const query = `SELECT * FROM "user" ORDER BY "last_name" ASC`;
   pool.query(query)
-  .then( result => {
-    res.send(result.rows);
-  })
-  .catch(err => {
-    console.log(`Error getting all users`, err);
-    res.sendStatus(500)
-  })
+    .then(result => {
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log(`Error getting all users`, err);
+      res.sendStatus(500)
+    })
 });
 
 // Updates user data 
 router.put('/:id', rejectUnauthenticated, (req, res) => {
-  console.log(req.body, req.params)
-
-  let id = req.params.id;
-
-  const query = `UPDATE "user"`
+  // console.log(req.body, req.params)
+  const userEdit = {
+    id: req.body.id,
+    category: req.body.category,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    phone_number: req.body.phone_number,
+    address: req.body.address,
+    city: req.body.city,
+    state: req.body.state,
+    zip: req.body.zip,
+    dob: req.body.dob,
+    involved_w_sond_since: req.body.involved_w_sond_since,
+    college_id: req.body.college_id,
+    password: req.body.password,
+    access_level: req.body.access_level,
+  }
+  const query = `
+  UPDATE "user
+  SET "category" = $1, "first_name" = $2, "last_name" = $3, "email" = $4, 
+  "phone_number" = $5, "address" = $6, "city" = $7, "state" = $8, 
+  "zip" = $9, "dob" = $10, "involved_w_sond_since" = $11,
+  "college_id" = $12, "password" = $13, "access_level" = $14`;
+  
 })
 
 
@@ -55,15 +75,15 @@ router.post('/register', (req, res, next) => {
     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING id`;
   pool
     .query(queryText, [req.body.category, req.body.first_name, req.body.last_name, email,
-      req.body.phone_number, req.body.address, req.body.city, req.body.state, req.body.zip, 
-      req.body.dob, req.body.involved_w_sond_since, 
-      req.body.college_id, password, req.body.access_level])
+    req.body.phone_number, req.body.address, req.body.city, req.body.state, req.body.zip,
+    req.body.dob, req.body.involved_w_sond_since,
+    req.body.college_id, password, req.body.access_level])
     .then(() => res.sendStatus(201))
     .catch((err) => {
       console.log('User registration failed: ', err);
       res.sendStatus(500);
     });
-    console.log(`User ${email} created`);
+  console.log(`User ${email} created`);
 });
 
 // Handles login form authenticate/login POST
