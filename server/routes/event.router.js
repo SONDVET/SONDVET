5e644b686b10ff3f8ff3ff8080c541d445f2228a
LanceLRG <Gagnerlance@gmail.com>
2021-03-19 09:57:54 -0500

    implemented simple event listing

--- server/routes/event.router.js
+++ server/routes/event.router.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
     const queryText = `SELECT * FROM "event";`;
     pool.query(queryText)
         .then(result => {
-            res.send(response.rows);
+            res.send(result.rows);
         }).catch(err => {
             console.log(err);
             res.sendStatus(500);
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
-import {useSelector} from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 //  This page is for users to view all events they subscribed to and edit their profile info.
 function UserPage() {
 
-
+  const dispatch = useDispatch();
+  const store = useSelector(store => store);
   const user = useSelector((store) => store.user);
 
+  useEffect(() => {
+    dispatch({ type: 'FETCH_EVENT' });
+  }, [])
 
   return (
     <>
-    <h1>User Page</h1>
-    <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-    </div>
+      <h1>User Page</h1>
+      <div className="container">
+        <h2>Welcome, {user.username}!</h2>
+        <p>Your ID is: {user.id}</p>
+      </div>
+      <div>
+        <ul>
+          {store.event[0] && store.event.map((event) => 
+          <li key={event.id}>{event.name}, {event.description}</li>)}
+        </ul>
+      </div>
     </>
   );
 }
