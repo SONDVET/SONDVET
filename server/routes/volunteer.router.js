f4d406a227d6536c931c161e6013f8567777743f
Cody Njos <cody.njos@live.com>
2021-03-19 09:20:54 -0500

    Gets all users by affilation id. Tested and Works in Postman

--- server/routes/volunteer.router.js
+++ server/routes/volunteer.router.js
@@ -19,5 +19,22 @@ router.get('/', rejectUnauthenticated, (req, res) => {
         })
 });
 
+// Selects All Users Who are in the affiliaton with the id
+// that is passed into the params
+router.get('/affiliation/:id' , (req, res) => {
+    const id = req.params.id
+    const queryText = `SELECT "user"."id","category" , "first_name" , "last_name" , "email" , "phone_number" , "address" , "city" , "state" , "zip" , "dob" , "involved_w_sond_since" , "college_id" , "access_level" 
+    FROM "user" 
+    WHERE "college_id" = ${id};`
+    pool.query(queryText)
+        .then(result => {
+            res.send(result.rows);
+        })
+        .catch(err => {
+            console.log(`Error getting all users`, err);
+            res.sendStatus(500)
+        })
+});
+
 
 module.exports = router;
