f3469ddfb7d8ef7816a17b6a5e1d596c5d2f9266
LanceLRG <Gagnerlance@gmail.com>
2021-03-22 10:56:56 -0500

    changed default stat in allUserEvent reducer

--- src/redux/reducers/allUserEvent.reducer.js
+++ src/redux/reducers/allUserEvent.reducer.js
@@ -1,9 +1,9 @@
-const AllUserEventReducer = (state = {}, action) => {
+const AllUserEventReducer = (state = [{id:0, user_id: 0, event_id: 0}], action) => {
     switch (action.type) {
       case 'SET_ALL_USER_EVENT':
         return action.payload;
       case 'UNSET_ALL_USER_EVENT':
-        return {};
+        return [{id:0, user_id: 0, event_id: 0}];
       default:
         return state;
     }
