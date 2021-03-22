3a4c34f9ede4382624c8523908014d5f5d6d75a1
LanceLRG <Gagnerlance@gmail.com>
2021-03-22 10:38:20 -0500

    maybe fix to event list bug

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -46,10 +46,10 @@ function Events() {
             <div className="eventListContainer">
                 <div>
                     {/* loops over every event in the event store and displays them in a div */}
-                    {(store.event[0] && store.allUserEvent[0]) && store.event.map((event) =>
+                    {(store.event[0]) && store.event.map((event) =>
                         <div key={event.id}> <img src={event.pic_url} height='50px' /> {event.name} {event.description} {event.location} {event.special_inst}
-                           {(checkForAttend(user.id, event.id)) && <button onClick={() => dispatch({type: 'ATTEND_EVENT', payload: {eventId: event.id, userId: user.id}})}>Join</button>}
-                           {(!checkForAttend(user.id, event.id)) &&<button onClick={() => dispatch({type: 'UNATTEND_EVENT', payload: {eventId: event.id, userId: user.id}})}>Can't make it</button>}
+                           {(checkForAttend(user.id, event.id) || !store.allUserEvent) && <button onClick={() => dispatch({type: 'ATTEND_EVENT', payload: {eventId: event.id, userId: user.id}})}>Join</button>}
+                           {(!checkForAttend(user.id, event.id) && store.allUserEvent) ? <button onClick={() => dispatch({type: 'UNATTEND_EVENT', payload: {eventId: event.id, userId: user.id}})}>Can't make it</button>: ''}
                             {(user.access_level >= 2) && <button onClick={() => goToDetails(event.id)}>Details</button>}
                         </div>)}
                 </div>
