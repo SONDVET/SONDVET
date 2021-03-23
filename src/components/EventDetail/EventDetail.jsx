4eb3b6e0d94ae309b241a808d20eb5a5716f0a35
LanceLRG <Gagnerlance@gmail.com>
2021-03-23 09:35:49 -0500

    moved remove button functionality to saga

--- src/components/EventDetail/EventDetail.jsx
+++ src/components/EventDetail/EventDetail.jsx
@@ -11,6 +11,7 @@ function EventDetail() {
     
     const params = useParams()
     const dispatch = useDispatch()
+    const user = useSelector ((store) => store.user)
     const userEvent = useSelector((store) => store.userEvent);
     const event = useSelector((store) => store.event);
     useEffect(() => {
@@ -18,16 +19,13 @@ function EventDetail() {
         dispatch({ type: 'FETCH_USER_EVENT', payload: params.id})
     }, []);
 
-    const removeVolunteer = (eventId, userId) => {
-        dispatch({type: 'UNATTEND_EVENT', payload: {eventId: eventId, userId: userId}});
-        dispatch({ type: 'FETCH_USER_EVENT', payload: params.id});
-    }
-
     return (
+        
         <>    
             {/* <button ><img src={InfoIcon}/></button>  onClick should toggle a modal to desribe use of check-in */}
         <div className="eventDetailContainer">
-          {event[0] &&
+          
+          {event[0] && user.access_level >= 2 ?
           <>
           <h1>{event[0].name}</h1>
           <img src = {event[0].pic_url}/>
@@ -35,15 +33,20 @@ function EventDetail() {
           <p>{event[0].description}</p>
           <p>{event[0].special_inst}</p>
           </>
+          :
+          <>
+          <h1>404</h1>
+          <h2>Not Found</h2>
+          </>
         }
-        {userEvent[0] && 
+        {userEvent[0] && user.access_level >= 2 && 
         <>
         <table className = "eventUser">
             <thead>
             <tr>
             <th>Name</th>
             <th>Role</th>
-            <th>Orginization</th>
+            <th>Organization</th>
             <th>Email</th>
             <th>Phone Number</th>
             <th colSpan="2">Check In / Out</th>
@@ -61,7 +64,7 @@ function EventDetail() {
                 <td>{user.phone_number}</td>
                 <td><button disabled={(user.check_in < user.check_out || user.check_in === null) ? false : true} onClick={() => dispatch({type: 'CHECK_IN', payload: {user_id: user.id, event_id: user.event_id, params: params.id}})}>Check In</button></td>
                 <td><button disabled={(user.check_in < user.check_out || user.check_in === null) ? true : false} onClick={() => dispatch({type: 'CHECK_OUT', payload: {user_id: user.id, event_id: user.event_id, params: params.id}})}>Check Out</button></td>
-                <td><button onClick={() => removeVolunteer(user.event_id, user.id)}>Remove</button></td>
+                <td><button onClick={() => dispatch({type: 'UNATTEND_EVENT', payload: {eventId: user.event_id, userId: user.id, params: params.id}})}>Remove</button></td>
                 </tr>
             )
         } )}
