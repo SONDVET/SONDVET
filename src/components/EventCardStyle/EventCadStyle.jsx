d2a6b47e0609e26c46c14652bcdbb35d848a7f89
Cody Njos <cody.njos@live.com>
2021-03-24 13:40:54 -0500

    minor style tweaks

--- src/components/EventCardStyle/EventCadStyle.jsx
+++ src/components/EventCardStyle/EventCadStyle.jsx
@@ -2,11 +2,14 @@ import { makeStyles } from "@material-ui/core"
 const useStyles = makeStyles({
     eventCard: {
         width: 400,
-        height: 250,
+        height: 300,
         margin: 10,
         paddingBottom: 10,
         backgroundColor: " rgb(223, 226, 230)",
         textAlign: "center"
+    },
+    searchBar: {
+        width:"65%",
     },
     
 });
