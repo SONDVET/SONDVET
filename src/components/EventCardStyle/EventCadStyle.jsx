0b174f2f1ac2ed6671d367b57997cdce9bd323d1
Cody Njos <cody.njos@live.com>
2021-03-29 14:30:18 -0500

    resised event cards to be percentage based

--- src/components/EventCardStyle/EventCadStyle.jsx
+++ src/components/EventCardStyle/EventCadStyle.jsx
@@ -1,11 +1,11 @@
 import { makeStyles } from "@material-ui/core"
 const useStyles = makeStyles({
     eventCard: {
-        width: 400,
+        minWidth: '25%',
         minHeight: 375,
         maxHeight: '100%',
         overflow: 'auto',
-        margin: 10,
+        margin: 10, 
         paddingBottom: 10,
         backgroundColor: " rgb(223, 226, 230)",
         textAlign: "center"
