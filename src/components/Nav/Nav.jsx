156bf41d9e51f1dbf0196072394c4028afd8d4c9
Cody Njos <cody.njos@live.com>
2021-03-29 17:34:55 -0500

    conditionally render nav bar based on screen width

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,7 +4,7 @@ import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 import { useSelector } from 'react-redux';
 import Logo from '../Images/WhiteLogo.png';  //src/components/Images/SONDLogo.png
-
+import {useMediaQuery} from '@material-ui/core'
 
 function Nav() {
   const user = useSelector((store) => store.user);
@@ -19,9 +19,11 @@ function Nav() {
     loginLinkData.text = 'My Profile';
   }
 
-
+  const shrink = useMediaQuery("(min-width: 800px)")
   return (
     <div className="nav">
+      {shrink ?
+      <>
       <Link to="/home">
         <img className="mainLogo" src={Logo} />
       </Link>
@@ -57,7 +59,47 @@ function Nav() {
           </Link>
           </>}
       </div>
+      </> 
+      :  
+      <>
+       {/* <Link to="/home">
+       <img className="mainLogo" src={Logo} />
+     </Link>  */}
+     <div>
+       <Link className="navLink" to={loginLinkData.path}>
+         {loginLinkData.text}
+       </Link>
+
+       {user.id && user.access_level >= 2 && (
+         <>
+           <Link className="navLink" to="/addevent">
+             Add Event
+       </Link>
+         </>
+       )}
+
+       <Link className="navLink" to="/events">
+         Events
+       </Link>
+
+       {user.id && (
+         <>
+           <Link className="navLink" to="/info">
+             Info Page
+           </Link>
+           <LogOutButton className="navLink" />
+         </>
+       )}
+       {user.id && user.access_level >= 3 &&
+         <>
+         <Link className="navLink" to="/admin">
+           Admin
+         </Link>
+         </>}
+     </div>
+     </>}
     </div>
+    
   );
 }
 
