c1f6fb6b32d6734be1a07ab9942fded57bdb64ec
Zach Mead <zachary.n.mead@gmail.com>
2021-03-29 12:02:56 -0500

    Moving unarchive

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -9,68 +9,72 @@ import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 
 function Admin() {
 
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const store = useSelector(store => store);
-   
-    const StyledTableCell = withStyles((theme) => ({
-        head: {
-          backgroundColor: theme.palette.common.black,
-          color: theme.palette.common.white,
-        },
-        body: {
-          fontSize: 14,
-        },
-      }))(TableCell);
-    
-      const StyledTableRow = withStyles((theme) => ({
-        root: {
-          '&:nth-of-type(odd)': {
-            backgroundColor: theme.palette.action.hover,
-          },
-        },
-      }))(TableRow);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const store = useSelector(store => store);
 
-    useEffect(() => {
-        dispatch({type: "FETCH_ARCHIVED"})
-      }, [])
+  const StyledTableCell = withStyles((theme) => ({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
+
+  const StyledTableRow = withStyles((theme) => ({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  }))(TableRow);
+
+  useEffect(() => {
+    dispatch({ type: "FETCH_ARCHIVED" })
+  }, [])
+
+  const unarchiveUser = () => {
+    dispatch({ type: 'UNARCHIVE_USER', payload: params.id });
+    history.push("/admin");
+  }
 
 
 
-
-    return(
-        <>
-        <h1>Archived Users</h1>
-        <TableContainer>
-            <Table>
-                <TableHead>
-                    <StyledTableRow>
-                        <StyledTableCell> Name </StyledTableCell>
-                        <StyledTableCell> Email </StyledTableCell>
-                        <StyledTableCell> Phone Number </StyledTableCell>
-                        <StyledTableCell> Address </StyledTableCell>
-                        <StyledTableCell> City </StyledTableCell>
-                        <StyledTableCell> State </StyledTableCell>
-                        <StyledTableCell> Zip Code </StyledTableCell>
-                    </StyledTableRow>
-                </TableHead>
-                <TableBody>
-                    {(store.archivedUsers[0] && store.archivedUsers.map((user) =>  
-                        <StyledTableRow key={user.id}>
-                        <StyledTableCell>{user.first_name} {user.last_name}</StyledTableCell>
-                        <StyledTableCell>{user.email}</StyledTableCell>
-                        <StyledTableCell>{user.phone_number}</StyledTableCell>
-                        <StyledTableCell>{user.address}</StyledTableCell>
-                        <StyledTableCell>{user.city}</StyledTableCell>
-                        <StyledTableCell>{user.state}</StyledTableCell>
-                        <StyledTableCell>{user.zip}</StyledTableCell>
-                        </StyledTableRow>
-                        ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-        </>
-    )
+  return (
+    <>
+      <h1>Archived Users</h1>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <StyledTableRow>
+              <StyledTableCell> Name </StyledTableCell>
+              <StyledTableCell> Email </StyledTableCell>
+              <StyledTableCell> Phone Number </StyledTableCell>
+              <StyledTableCell> Address </StyledTableCell>
+              <StyledTableCell> City </StyledTableCell>
+              <StyledTableCell> State </StyledTableCell>
+              <StyledTableCell> Zip Code </StyledTableCell>
+            </StyledTableRow>
+          </TableHead>
+          <TableBody>
+            {(store.archivedUsers[0] && store.archivedUsers.map((user) =>
+              <StyledTableRow key={user.id}>
+                <StyledTableCell>{user.first_name} {user.last_name}</StyledTableCell>
+                <StyledTableCell>{user.email}</StyledTableCell>
+                <StyledTableCell>{user.phone_number}</StyledTableCell>
+                <StyledTableCell>{user.address}</StyledTableCell>
+                <StyledTableCell>{user.city}</StyledTableCell>
+                <StyledTableCell>{user.state}</StyledTableCell>
+                <StyledTableCell>{user.zip}</StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  )
 }
 
 export default Admin
