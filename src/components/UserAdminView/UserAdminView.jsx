9a9984a18c84f9409c3cf6f77a4355ffb94f7336
Zach Mead <zachary.n.mead@gmail.com>
2021-03-29 18:29:11 -0500

    Lotsa Errorz

--- src/components/UserAdminView/UserAdminView.jsx
+++ src/components/UserAdminView/UserAdminView.jsx
@@ -2,6 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import "./UserAdminView.css";
 import { useHistory, useParams } from 'react-router-dom'
+import { Table, TableContainer, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
+import SaveIcon from '@material-ui/icons/Save';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 
 //  This page is for users to view all events they subscribed to and edit their profile info.
 function UserAdminView() {
@@ -125,105 +133,158 @@ function UserAdminView() {
 
     return (
         <>
+            <Grid container spacing={1} direction="row" justify="space-around">
+                <div className="container">
+                    <h2>{user.last_name}, {user.first_name}</h2>
+                    <p>{user.email}</p>
+                </div>
 
-
-            {store.oneUser[0] && store.user.access_level === 3 ?
-                <>
-                    <h1>Admin Page</h1>
-                    <div className="container">
-                        <h2>{user.last_name}, {user.first_name}</h2>
-                        <p>{user.email}</p>
-                    </div>
-
-                    {(store.user.access_level > 2) &&
+                {(user.access_level > 2) &&
+                    <Grid item>
                         <div className="rankContainer">
-                            <div>Rank:</div>
-                            <div><i>{edit ? <div>{accessRanks[user.access_level - 1]}</div>
-                                : <select defaultValue={user.access_level} onChange={(e) => setPerson({ ...person, access_level: e.target.value })}>
-                                    <option value="1">Volunteer</option>
-                                    <option value="2">Officer</option>
-                                    <option value="3">Admin</option>
-                                </select>}
-                            </i>
-                            </div>
-                        </div>}
+                            <div>Rank: &nbsp;</div>  <div clasname="rank">{edit ? <div><b>{accessRanks[user.access_level - 1]}</b></div> : <select defaultValue={user.access_level} onChange={(e) => setPerson({ ...person, access_level: e.target.value })}><option value="1">Volunteer</option><option value="2">Officer</option><option value="3">Admin</option></select>}</div>
+                        </div></Grid>}
+                <Grid item>
+                    <Button variant="contained" color="default" onClick={() => setEditMode()}>{edit ? 'Edit Info' : 'cancel edit'}</Button> {!edit ? <Button variant="contained" color="primary" startIcon={<SaveIcon />} onClick={() => updateInfo()} >save</Button> : ''}
+                </Grid>
+            </Grid>
+            <br />
+            <Grid container direction="row" justify="space-evenly" alignItems="center">
+                {/* Personal Information Table */}
+                <Grid item lg={5}>
+                    <TableContainer className='personalInfoContainer' component={Paper} >
+                        <Table size="small">
+                            <TableHead>
+                                <StyledTableRow>
+                                    <StyledTableCell colSpan="2" align="center">Personal Information</StyledTableCell>
+                                </StyledTableRow>
+                            </TableHead>
+                            <TableBody>
+                                <StyledTableRow>
+                                    <StyledTableCell><b>Category</b></StyledTableCell>
+                                    <StyledTableCell>{edit ? <div>{user.category}</div> : <input defaultValue={user.category} onChange={(e) => setPerson({ ...person, category: e.target.value })} />}</StyledTableCell>
+                                </StyledTableRow>
+                                <StyledTableRow>
+                                    <StyledTableCell><b>First Name</b></StyledTableCell>
+                                    <StyledTableCell>{edit ? <div>{user.first_name}</div> : <input defaultValue={user.first_name} onChange={(e) => setPerson({ ...person, first_name: e.target.value })} />}</StyledTableCell>
+                                </StyledTableRow>
+                                <StyledTableRow>
+                                    <StyledTableCell><b>Last Name</b></StyledTableCell>
+                                    <StyledTableCell>{edit ? <div>{user.last_name}</div> : <input defaultValue={user.last_name} onChange={(e) => setPerson({ ...person, last_name: e.target.value })} />}</StyledTableCell>
+                                </StyledTableRow>
+                                <StyledTableRow>
+                                    <StyledTableCell><b>Email</b></StyledTableCell>
+                                    <StyledTableCell>{edit ? <div>{user.email}</div> : <input defaultValue={user.email} onChange={(e) => setPerson({ ...person, username: e.target.value })} type="email" />}</StyledTableCell>
+                                </StyledTableRow>
+                                <StyledTableRow>
+                                    <StyledTableCell><b>Phone</b></StyledTableCell>
+                                    <StyledTableCell>{edit ? <div>{user.phone_number}</div> : <input defaultValue={user.phone_number.split('-').join('')} onChange={(e) => setPerson({ ...person, phone_number: e.target.value })} type="number" />}</StyledTableCell>
+                                </StyledTableRow>
+                                <StyledTableRow>
+                                    <StyledTableCell><b>Address</b></StyledTableCell>
+                                    <StyledTableCell>{edit ? <div>{user.address}</div> : <input defaultValue={user.address} onChange={(e) => setPerson({ ...person, address: e.target.value })} />}</StyledTableCell>
+                                </StyledTableRow>
+                                <StyledTableRow>
+                                    <StyledTableCell><b>City</b></StyledTableCell>
+                                    <StyledTableCell>{edit ? <div>{user.city}</div> : <input defaultValue={user.city} onChange={(e) => setPerson({ ...person, city: e.target.value })} />}</StyledTableCell>
+                                </StyledTableRow>
+                                <StyledTableRow>
+                                    <StyledTableCell><b>State</b></StyledTableCell>
+                                    <StyledTableCell>{edit ? <div>{user.state}</div> : <input defaultValue={user.state} onChange={(e) => setPerson({ ...person, state: e.target.value })} />}</StyledTableCell>
+                                </StyledTableRow>
+                                <StyledTableRow>
+                                    <StyledTableCell><b>Zip</b></StyledTableCell>
+                                    <StyledTableCell>{edit ? <div>{user.zip}</div> : <input defaultValue={user.zip} onChange={(e) => setPerson({ ...person, zip: e.target.value })} type="number" />}</StyledTableCell>
+                                </StyledTableRow>
+                                <StyledTableRow>
+                                    <StyledTableCell><b>Date of Birth</b></StyledTableCell>
+                                    <StyledTableCell>{edit ? <div>{user.dob.substring(0, 10)}</div> : <input defaultValue={user.dob.substring(0, 10)} onChange={(e) => setPerson({ ...person, dob: e.target.value })} type="date" />}</StyledTableCell>
+                                </StyledTableRow>
+                                <StyledTableRow>
+                                    <StyledTableCell><b>Involved with SOND Since</b></StyledTableCell>
+                                    <StyledTableCell>{edit ? <div>{user.involved_w_sond_since.substring(0, 10)}</div> : <input defaultValue={user.involved_w_sond_since.substring(0, 10)} onChange={(e) => setPerson({ ...person, involved_w_sond_since: e.target.value })} type="date" />}</StyledTableCell>
+                                </StyledTableRow>
+                            </TableBody>
+                        </Table>
 
-                    <h3>Personal Information:</h3>
-                    <div className='personalInfoContainer'>
-                        <div className="personInfoItem">Category</div>                 <div className="personInfoItem">{edit ? <div>{user.category}</div> : <input defaultValue={user.category} onChange={(e) => setPerson({ ...person, category: e.target.value })} />}</div>
-                        <div className="personInfoItem">First Name</div>               <div className="personInfoItem">{edit ? <div>{user.first_name}</div> : <input defaultValue={user.first_name} onChange={(e) => setPerson({ ...person, first_name: e.target.value })} />}</div>
-                        <div className="personInfoItem">Last Name</div>                <div className="personInfoItem">{edit ? <div>{user.last_name}</div> : <input defaultValue={user.last_name} onChange={(e) => setPerson({ ...person, last_name: e.target.value })} />}</div>
-                        <div className="personInfoItem">Email</div>                    <div className="personInfoItem">{edit ? <div>{user.email}</div> : <input defaultValue={user.email} onChange={(e) => setPerson({ ...person, username: e.target.value })} type="email" />}</div>
-                        <div className="personInfoItem">Phone</div>                    <div className="personInfoItem">{edit ? <div>{user.phone_number}</div> : <input defaultValue={user.phone_number.split('-').join('')} onChange={(e) => setPerson({ ...person, phone_number: e.target.value })} type="number" />}</div>
-                        <div className="personInfoItem">Address</div>                  <div className="personInfoItem">{edit ? <div>{user.address}</div> : <input defaultValue={user.address} onChange={(e) => setPerson({ ...person, address: e.target.value })} />}</div>
-                        <div className="personInfoItem">City</div>                     <div className="personInfoItem">{edit ? <div>{user.city}</div> : <input defaultValue={user.city} onChange={(e) => setPerson({ ...person, city: e.target.value })} />}</div>
-                        <div className="personInfoItem">State</div>                    <div className="personInfoItem">{edit ? <div>{user.state}</div> : <input defaultValue={user.state} onChange={(e) => setPerson({ ...person, state: e.target.value })} />}</div>
-                        <div className="personInfoItem">Zip</div>                      <div className="personInfoItem">{edit ? <div>{user.zip}</div> : <input defaultValue={user.zip} onChange={(e) => setPerson({ ...person, zip: e.target.value })} type="number" />}</div>
-                        <div className="personInfoItem">Date of Birth</div>            <div className="personInfoItem">{edit ? <div>{user.dob.substring(0, 10)}</div> : <input value={user.dob.substring(0, 10)} onChange={(e) => setPerson({ ...person, dob: e.target.value })} type="date" />}</div>
-                        <div className="personInfoItem">Involved with SOND Since</div> <div className="personInfoItem">{edit ? <div>{user.involved_w_sond_since.substring(0, 10)}</div> : <input value={user.involved_w_sond_since.substring(0, 10)} onChange={(e) => setPerson({ ...person, involved_w_sond_since: e.target.value })} type="date" />}</div>
-                        <div className="personInfoItem">Affiliation/College</div>      <div className="personInfoItem">{edit ? <div>{user.college_id}</div> : <input defaultValue={user.college_id} onChange={(e) => setPerson({ ...person, college_id: e.target.value })} type="number" />}</div>
-                        <div className="personInfoItem">Archived</div>                <div className="personInfoItem">{edit ? <div>{user.archived.toString()}</div> : <input defaultValue={user.archived.toString()} onChange={(e) => setPerson({ ...person, archived: e.target.value })}/>}</div>
-                        {/* edit button will conidtionally render the divs into inputs, Save will dispatch the new data */}
-                        <button onClick={() => setEditMode()}>Edit Info</button> 
-                        <button onClick={() => updateInfo()}>Save</button>
-                        <button onClick={() => archiveUser()}>Archive User</button>
-                        <button onClick={() => unarchiveUser()}>Restore User</button>
-                    </div>
-                    <div className="userEventsContainer">
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <th>
-                                        Event
-                                    </th>
-                                    <th>
-                                        Event Date
-                                    </th>
-                                    <th>
-                                        Attendance
-                                    </th>
-                                    <th>
-                                        {/* purposefully empty*/}
-                                    </th>
-                                </tr>
-                                {(store.oneUserEvent[1]) && store.oneUserEvent.map((item) => <tr key={item.id}>
-                                    <td>
-                                        {item.name}
-                                    </td>
-                                    <td>
+                    </TableContainer>
+                </Grid>
+
+                {/* Group Table */}
+                <Grid item md={4}>
+                    <TableContainer component={Paper}>
+                        <Table>
+                            <TableHead>
+                                <StyledTableRow>
+                                    <StyledTableCell colSpan="3" align="center">Groups</StyledTableCell>
+                                </StyledTableRow>
+                            </TableHead>
+                            <TableBody>
+                                <StyledTableRow>
+                                    <StyledTableCell><b>Name</b></StyledTableCell>
+                                    <StyledTableCell><b>Joined?</b></StyledTableCell>
+                                    <StyledTableCell></StyledTableCell>
+                                </StyledTableRow>
+                                {(store.affiliate[0]) && store.affiliate.map((group) => <StyledTableRow key={group.id}>
+                                    <StyledTableCell><b>{group.college_name}</b></StyledTableCell>
+                                    <StyledTableCell>{(store.userGroup[0]) && isAMember(user.id, group.id) ? <CheckCircleIcon color='primary' /> : <HighlightOffIcon />} </StyledTableCell>
+                                    <StyledTableCell>{((store.userGroup[0]) && isAMember(user.id, group.id) ? <button onClick={() => toggleJoin(user.id, group.id, 'leave')}>Leave</button> : <button onClick={() => toggleJoin(user.id, group.id, 'join')}>Join</button>)}</StyledTableCell>
+                                </StyledTableRow>)}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+            </Grid>
+            <br />
+
+            {/* User Events Table */}
+            <Grid container direction="row" justify="space-evenly" alignItems="center">
+                <Grid item xs={10}>
+                    <TableContainer component={Paper} >
+                        <Table>
+                            <TableHead>
+                                <StyledTableRow>
+                                    <StyledTableCell align="left">Event</StyledTableCell>
+                                    <StyledTableCell align="center">Event Date</StyledTableCell>
+                                    <StyledTableCell align="center">Attendance</StyledTableCell>
+                                    <StyledTableCell align="center">View</StyledTableCell>
+                                </StyledTableRow>
+                            </TableHead>
+                            <TableBody>
+                                {(store.oneUserEvent[1]) && store.oneUserEvent.map((item) => <StyledTableRow key={item.id}>
+                                    <StyledTableCell align="left">
+                                        <b>{item.name}</b>
+                                    </StyledTableCell>
+                                    <StyledTableCell align="center">
                                         {item.date.substring(0, 10)}
-                                    </td>
-                                    <td>
-                                        {(item.total_time.hours) ? (`${item.total_time.hours} hours`) : ''}
+                                    </StyledTableCell>
+                                    <StyledTableCell align="center">
+                                        {(item.total_time.hours) ? (`${item.total_time.hours} hours `) : ''}
                                         {(item.total_time.minutes) ? (`${item.total_time.minutes} minutes`) : 'N/A'}
-                                    </td>
-                                    <td>
+                                    </StyledTableCell >
+                                    <StyledTableCell align="center">
                                         <button onClick={() => history.push(`/details/${item.event_id}`)}>view event</button>
-                                    </td>
-                                </tr>)}
-                                <tr>
-                                    <td colSpan="4">
+                                    </StyledTableCell>
+                                </StyledTableRow>)}
+                                <StyledTableRow>
+                                    <StyledTableCell colSpan="4">
                                         {/*intentionally blank buffer column */}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        Total Volunteer Time:
-                                    </td>
-                                    <td colSpan="3">
+                                    </StyledTableCell>
+                                </StyledTableRow>
+                                <StyledTableRow>
+                                    <StyledTableCell>
+                                        <b>Total Volunteer Time:</b>
+                                    </StyledTableCell>
+                                    <StyledTableCell colSpan="3" align="center">
                                         {grandTotalHours} hours {grandTotalMinutes} minutes
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </>
-                :
-                <>
-                    <h1>404</h1>
-                    <h1>Not Found</h1>
-                </>
-            }
+                                    </StyledTableCell>
+                                </StyledTableRow>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+            </Grid>
+
         </>
     );
 }
