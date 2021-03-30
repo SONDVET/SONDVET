8b90cd942bd126f1fd665e020695c507e74b4f7d
JDK <jdkranzler@yahoo.com>
2021-03-30 11:38:23 -0500

    fixed the edit, phone numbers formatted

--- src/components/GroupView/GroupView.jsx
+++ src/components/GroupView/GroupView.jsx
@@ -96,6 +96,15 @@ function GroupView() {
         setUserOpen(false);
     };
 
+    const phoneFormater = (phoneNumb) => {
+        let format = ('' + phoneNumb).replace(/\D/g, '');
+        let match = format.match(/^(\d{3})(\d{3})(\d{4})$/);
+        if (match) {
+            return '(' + match[1] + ')' + match[2] + '-' + match[3];
+        }
+        return null;
+    }
+
 
     return (
         <>
@@ -145,7 +154,7 @@ function GroupView() {
                             <StyledTableCell>{affiliates.last_name}</StyledTableCell>
                             <StyledTableCell>{affiliates.first_name}</StyledTableCell>
                             <StyledTableCell>{affiliates.email}</StyledTableCell>
-                            <StyledTableCell>{affiliates.phone_number}</StyledTableCell>
+                            <StyledTableCell>{phoneFormater(affiliates.phone_number)}</StyledTableCell>
                             <StyledTableCell align="center"><button onClick={() => goToUser(affiliates.id)}>View</button></StyledTableCell>
                             {/* <StyledTableCell align="center"><button onClick={() => dispatch({ type: 'REMOVE_USER_GROUP', payload: { user_id: affiliates.id, group_id: affiliates.group_id, parameter: params.id } })}>Remove</button></StyledTableCell> */}
                             <StyledTableCell>
