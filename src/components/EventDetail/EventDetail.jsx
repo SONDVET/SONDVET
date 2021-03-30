611564778c2239bfcacad884e196bea339735650
LanceLRG <Gagnerlance@gmail.com>
2021-03-30 15:47:21 -0500

    put event details into a card

--- src/components/EventDetail/EventDetail.jsx
+++ src/components/EventDetail/EventDetail.jsx
@@ -14,8 +14,10 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { useTheme } from '@material-ui/core/styles';
-import { Table, TableContainer, TableHead, TableRow, TableCell, TableBody, Container, Grid, Paper } from '@material-ui/core';
+import { Card, CardMedia, CardHeader, CardContent, CardActions, CardActionsArea, TextField, Table, TableContainer, TableHead, TableRow, TableCell, TableBody, Container, Grid, Paper } from '@material-ui/core';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { useStyles } from '../EventCardStyle/EventCadStyle'
+
 
 
 //Styling for material tables
@@ -54,6 +56,7 @@ function EventDetail() {
     const userEvent = useSelector((store) => store.userEvent);
     const event = useSelector((store) => store.event);
     const today = new Date();
+
     useEffect(() => {
         dispatch({ type: 'FETCH_EVENT_DETAILS', payload: params.id });
         dispatch({ type: 'FETCH_USER_EVENT', payload: params.id })
@@ -89,89 +92,107 @@ function EventDetail() {
         return null;
     }
 
+    const shrink = useMediaQuery("(min-width: 800px)")
+    const classes = useStyles()
+
     return (
-        <>
+        <>{(event[0] && userEvent[0]) &&
             <Container>
                 {/* <button ><img src={InfoIcon}/></button>  onClick should toggle a modal to desribe use of check-in */}
-                            <h1>{event[0].name}</h1>
-                            <img src={event[0].pic_url} height='150px'/>
-                            <p>{moment(event[0].date).format('MMMM Do YYYY')}</p>
-                            <p>{event[0].time}</p>
-                            {/* <p>{time}</p> */}
-                            <p>{event[0].description}</p>
-                            <p>{event[0].special_inst}</p>
+                <Card className={classes.mobileCard}>
+                    <CardHeader title={event[0].name} />
+                    <CardContent>
+                        <img src={event[0].pic_url} height='150px' />
+                    </CardContent>
+                    <CardContent className="descriptionText" >
+                        <Grid container justify="space-around">
+                            <Grid item>
+                                <p><b>WHEN:</b></p>
+                                <p>{moment(event[0].date).format('MMMM Do YYYY')}</p>
+                                <p>{moment(event[0].time, "HH:mm").format('hh:mm A')}</p>
+                            </Grid>
+                            <Grid item>
+                                <p><b>WHERE:</b></p>
+                                <p >{event[0].location}</p>
+                            </Grid>
+                        </Grid>
+                        <p>{event[0].description}</p>
+                        <p>{event[0].special_inst}</p>
+                    </CardContent>
+                </Card>
 
-                            <ReactHTMLTableToExcel
-                                id="test-table-xls-button"
-                                className="download-table-xls-button"
-                                table="eventUser"
-                                filename="Event Registrants"
-                                sheet="eventUser.xls"
-                                buttonText="Download Event Registrants" />
-                                <h2>Scheduled Participants</h2>
-                            <TableContainer component={Paper} >
-                                <Table id="eventUser" className="eventUser">
-                                    <TableHead>
-                                        <StyledTableRow>
-                                            <StyledTableCell>Name</StyledTableCell>
-                                            <StyledTableCell>Role</StyledTableCell>
-                                            <StyledTableCell>Organization</StyledTableCell>
-                                            <StyledTableCell>Email</StyledTableCell>
-                                            <StyledTableCell>Phone Number</StyledTableCell>
-                                            <StyledTableCell colSpan="2">Check In / Out</StyledTableCell>
-                                            <StyledTableCell>Remove</StyledTableCell>
-                                        </StyledTableRow>
-                                    </TableHead>
-                                    <tbody>
-                                        {userEvent.map(user => {
-                                            return (
-                                                <StyledTableRow key={user.id}>
-                                                    <StyledTableCell>{user.first_name} {user.last_name}</StyledTableCell>
-                                                    <StyledTableCell>{user.category}</StyledTableCell>
-                                                    <StyledTableCell>{user.college_name}</StyledTableCell>
-                                                    <StyledTableCell>{user.email}</StyledTableCell>
-                                                    <StyledTableCell>{phoneFormater(user.phone_number)}</StyledTableCell>
-                                                    <StyledTableCell><button disabled={(user.check_in < user.check_out || user.check_in === null) ? false : true} onClick={() => dispatch({ type: 'CHECK_IN', payload: { user_id: user.id, event_id: user.event_id, params: params.id } })}>Check In</button></StyledTableCell>
-                                                    <StyledTableCell><button disabled={(user.check_in < user.check_out || user.check_in === null) ? true : false} onClick={() => dispatch({ type: 'CHECK_OUT', payload: { user_id: user.id, event_id: user.event_id, params: params.id } })}>Check Out</button></StyledTableCell>
-                                                    <StyledTableCell><button onClick={() => dispatch({ type: 'UNATTEND_EVENT', payload: { eventId: user.event_id, userId: user.id, params: params.id } })}>Remove</button></StyledTableCell>
-                                                </StyledTableRow>
-                                            )
-                                        })}
-                                    </tbody>
-                                </Table>
-                            </TableContainer>
+                <ReactHTMLTableToExcel
+                    id="test-table-xls-button"
+                    className="download-table-xls-button"
+                    table="eventUser"
+                    filename="Event Registrants"
+                    sheet="eventUser.xls"
+                    buttonText="Download Event Registrants" />
+                <h2>Scheduled Participants</h2>
+                <TableContainer component={Paper} >
+                    <Table id="eventUser" className="eventUser">
+                        <TableHead>
+                            <StyledTableRow>
+                                <StyledTableCell>Name</StyledTableCell>
+                                <StyledTableCell>Role</StyledTableCell>
+                                <StyledTableCell>Organization</StyledTableCell>
+                                <StyledTableCell>Email</StyledTableCell>
+                                <StyledTableCell>Phone Number</StyledTableCell>
+                                <StyledTableCell colSpan="2">Check In / Out</StyledTableCell>
+                                <StyledTableCell>Remove</StyledTableCell>
+                            </StyledTableRow>
+                        </TableHead>
+                        <tbody>
+                            {userEvent.map(user => {
+                                return (
+                                    <StyledTableRow key={user.id}>
+                                        <StyledTableCell>{user.first_name} {user.last_name}</StyledTableCell>
+                                        <StyledTableCell>{user.category}</StyledTableCell>
+                                        <StyledTableCell>{user.college_name}</StyledTableCell>
+                                        <StyledTableCell>{user.email}</StyledTableCell>
+                                        <StyledTableCell>{phoneFormater(user.phone_number)}</StyledTableCell>
+                                        <StyledTableCell><button disabled={(user.check_in < user.check_out || user.check_in === null) ? false : true} onClick={() => dispatch({ type: 'CHECK_IN', payload: { user_id: user.id, event_id: user.event_id, params: params.id } })}>Check In</button></StyledTableCell>
+                                        <StyledTableCell><button disabled={(user.check_in < user.check_out || user.check_in === null) ? true : false} onClick={() => dispatch({ type: 'CHECK_OUT', payload: { user_id: user.id, event_id: user.event_id, params: params.id } })}>Check Out</button></StyledTableCell>
+                                        <StyledTableCell><button onClick={() => dispatch({ type: 'UNATTEND_EVENT', payload: { eventId: user.event_id, userId: user.id, params: params.id } })}>Remove</button></StyledTableCell>
+                                    </StyledTableRow>
+                                )
+                            })}
+                        </tbody>
+                    </Table>
+                </TableContainer>
 
-                            {event[0] && user.access_level >= 2 &&
-                                // <button onClick={() => archiveEvent()}>Archive Event</button>
-                                <div>
-                                    <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-                                        Delete Event
+                {event[0] && user.access_level >= 2 &&
+                    // <button onClick={() => archiveEvent()}>Archive Event</button>
+                    <div>
+                        <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+                            Delete Event
                         </Button>
-                                    <Dialog
-                                        fullScreen={fullScreen}
-                                        open={open}
-                                        onClose={handleClose}
-                                        aria-labelledby="responsive-dialog-title"
-                                    >
-                                        <DialogTitle id="responsive-dialog-title">{"Are you sure?"}</DialogTitle>
-                                        <DialogContent>
-                                            <DialogContentText>
-                                                Are you sure you want to delete this event?  If you do they will be set to "archived" and only Admins will be able to retrive them.
+                        <Dialog
+                            fullScreen={fullScreen}
+                            open={open}
+                            onClose={handleClose}
+                            aria-labelledby="responsive-dialog-title"
+                        >
+                            <DialogTitle id="responsive-dialog-title">{"Are you sure?"}</DialogTitle>
+                            <DialogContent>
+                                <DialogContentText>
+                                    Are you sure you want to delete this event?  If you do they will be set to "archived" and only Admins will be able to retrive them.
                             </DialogContentText>
-                                        </DialogContent>
-                                        <DialogActions>
-                                            <Button autoFocus onClick={handleClose} color="primary">
-                                                Disagree and Cancel
+                            </DialogContent>
+                            <DialogActions>
+                                <Button autoFocus onClick={handleClose} color="primary">
+                                    Disagree and Cancel
                                 </Button>
-                                            <Button onClick={handleClose, archiveEvent} color="primary" autoFocus>
-                                                Agree and Archive Event
+                                <Button onClick={handleClose, archiveEvent} color="primary" autoFocus>
+                                    Agree and Archive Event
                                 </Button>
-                                        </DialogActions>
-                                    </Dialog>
-                                </div>
-                                // <button onClick={() => unarchiveEvent()}>Unarchive Event</button>
-                            }
+                            </DialogActions>
+                        </Dialog>
+                    </div>
+                    // <button onClick={() => unarchiveEvent()}>Unarchive Event</button>
+                }
             </Container>
+        }
         </>
     );
 }
