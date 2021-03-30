bf57189e38bd5c86e2f0c59d22a6b26db7a156df
LanceLRG <Gagnerlance@gmail.com>
2021-03-30 09:53:28 -0500

    changed buttons to material buttons

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -159,7 +159,7 @@ function Events() {
                             <StyledTableRow key={affiliate.id}>
                                 <StyledTableCell>{affiliate.college_name}</StyledTableCell>
                                 <StyledTableCell>{(store.userGroup[0]) && memberCount(affiliate.id)}</StyledTableCell>
-                                <StyledTableCell><button onClick={() => goToGroup(affiliate.id)}>View</button></StyledTableCell>
+                                <StyledTableCell><Button variant="contained" onClick={() => goToGroup(affiliate.id)}>View</Button></StyledTableCell>
                             </StyledTableRow>
                         )}
                     </TableBody>
