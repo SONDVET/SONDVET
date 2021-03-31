e7eee39902c0aa5659791e3fa63b4d5e500a2bbe
LanceLRG <Gagnerlance@gmail.com>
2021-03-31 09:42:17 -0500

    centered header text on add event form

--- src/components/AddEvent/AddEvent.jsx
+++ src/components/AddEvent/AddEvent.jsx
@@ -49,98 +49,98 @@ function AddEvent() {
     return (
         <>
             <Container>
-                <h1>Fill out this form to create a new event.</h1>
                 <div className="addEventContainer">
+                    <h1 className="registerHeader">Fill out this form to create a new event.</h1>
                     <form className="addEventForm" onSubmit={addNewEvent}>
                         <Grid container direction="row" justify="center">
                             <Grid item>
-                            <div className="upperFields">
-                                <TextField
-                                    className={classes.input}
-                                    id="name-control"
-                                    label="Name"
-                                    variant="outlined"
-                                    inputProps={{ maxLength: 255 }}
-                                    required
-                                    style={{ width: 420 }}
-                                    onChange={(e) => setNewEvent({ ...newEvent, name: e.target.value })}
-                                />
-                                <TextField
-                                    className={classes.description}
-                                    id="description-control"
-                                    label="Description"
-                                    variant="outlined"
-                                    inputProps={{ maxLength: 255 }}
-                                    multiline
-                                    rows={6}
-                                    required
-                                    onChange={(e) => setNewEvent({ ...newEvent, description: e.target.value })}
-                                />
-                                <TextField
-                                    className={classes.input}
-                                    id="sp-instruction-control"
-                                    label="Special Instructions"
-                                    variant="outlined"
-                                    inputProps={{ maxLength: 255 }}
-                                    multiline
-                                    rows={6}
-                                    helperText="optional"
-                                    style={{ width: 420 }}
-                                    onChange={(e) => setNewEvent({ ...newEvent, special_inst: e.target.value })}
-                                />
-                            </div>
+                                <div className="upperFields">
+                                    <TextField
+                                        className={classes.input}
+                                        id="name-control"
+                                        label="Name"
+                                        variant="outlined"
+                                        inputProps={{ maxLength: 255 }}
+                                        required
+                                        style={{ width: 420 }}
+                                        onChange={(e) => setNewEvent({ ...newEvent, name: e.target.value })}
+                                    />
+                                    <TextField
+                                        className={classes.description}
+                                        id="description-control"
+                                        label="Description"
+                                        variant="outlined"
+                                        inputProps={{ maxLength: 255 }}
+                                        multiline
+                                        rows={6}
+                                        required
+                                        onChange={(e) => setNewEvent({ ...newEvent, description: e.target.value })}
+                                    />
+                                    <TextField
+                                        className={classes.input}
+                                        id="sp-instruction-control"
+                                        label="Special Instructions"
+                                        variant="outlined"
+                                        inputProps={{ maxLength: 255 }}
+                                        multiline
+                                        rows={6}
+                                        helperText="optional"
+                                        style={{ width: 420 }}
+                                        onChange={(e) => setNewEvent({ ...newEvent, special_inst: e.target.value })}
+                                    />
+                                </div>
                             </Grid>
                             <Grid item>
-                            <div className="upperFields">
-                                <TextField
-                                    className={classes.input}
-                                    inputProps={{ maxLength: 255 }}
-                                    id="location-control"
-                                    label="Location"
-                                    variant="outlined"
-                                    multiline
-                                    rowsMax={4}
-                                    required
-                                    onChange={(e) => setNewEvent({ ...newEvent, location: e.target.value })}
-                                />
-                                <TextField
-                                    className={classes.input}
-                                    id="date-control"
-                                    label="Date"
-                                    InputLabelProps={{ shrink: true }}
-                                    variant="outlined"
-                                    type="date"
-                                    rowsMax={4}
-                                    required
-                                    onChange={(e) => setNewEvent({ ...newEvent, date: e.target.value })}
-                                />
-                                <TextField
-                                    className={classes.input}
-                                    id="date-control"
-                                    label="Time"
-                                    InputLabelProps={{ shrink: true }}
-                                    variant="outlined"
-                                    type="time"
-                                    rowsMax={4}
-                                    required
-                                    onChange={(e) => setNewEvent({ ...newEvent, time: e.target.value })}
-                                />
-                                <TextField
-                                    className={classes.input}
-                                    id="date-control"
-                                    label="Image URL"
-                                    variant="outlined"
-                                    inputProps={{ maxLength: 2550 }}
-                                    rowsMax={4}
-                                    helperText="optional"
-                                    onChange={(e) => setNewEvent({ ...newEvent, pic_url: e.target.value })}
-                                />
+                                <div className="upperFields">
+                                    <TextField
+                                        className={classes.input}
+                                        inputProps={{ maxLength: 255 }}
+                                        id="location-control"
+                                        label="Location"
+                                        variant="outlined"
+                                        multiline
+                                        rowsMax={4}
+                                        required
+                                        onChange={(e) => setNewEvent({ ...newEvent, location: e.target.value })}
+                                    />
+                                    <TextField
+                                        className={classes.input}
+                                        id="date-control"
+                                        label="Date"
+                                        InputLabelProps={{ shrink: true }}
+                                        variant="outlined"
+                                        type="date"
+                                        rowsMax={4}
+                                        required
+                                        onChange={(e) => setNewEvent({ ...newEvent, date: e.target.value })}
+                                    />
+                                    <TextField
+                                        className={classes.input}
+                                        id="date-control"
+                                        label="Time"
+                                        InputLabelProps={{ shrink: true }}
+                                        variant="outlined"
+                                        type="time"
+                                        rowsMax={4}
+                                        required
+                                        onChange={(e) => setNewEvent({ ...newEvent, time: e.target.value })}
+                                    />
+                                    <TextField
+                                        className={classes.input}
+                                        id="date-control"
+                                        label="Image URL"
+                                        variant="outlined"
+                                        inputProps={{ maxLength: 2550 }}
+                                        rowsMax={4}
+                                        helperText="optional"
+                                        onChange={(e) => setNewEvent({ ...newEvent, pic_url: e.target.value })}
+                                    />
                                 </div>
                             </Grid>
                         </Grid>
 
                         <div className="buttonBox">
-                             <Button type="reset" variant="contained" color="secondary">Clear Fields</Button>
+                            <Button type="reset" variant="contained" color="secondary">Clear Fields</Button>
                             <Button type="submit" variant="contained" color="primary">Submit</Button>
                         </div>
                         <FormHelperText id="my-helper-text">*Fields with an asterisk are required</FormHelperText>
