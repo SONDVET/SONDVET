a146b0ceabefb6e26b55ecc9e67b7da693b8f279
LanceLRG <Gagnerlance@gmail.com>
2021-04-02 15:39:21 -0500

    changed default pages for userview and adminuserview tables to 10

--- src/components/UserAdminView/UserAdminView.jsx
+++ src/components/UserAdminView/UserAdminView.jsx
@@ -129,7 +129,7 @@ function UserAdminView() {
     }
 
     const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
   
     const handleChangePage = (event, newPage) => {
       setPage(newPage);
